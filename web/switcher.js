9743f96096c9b4c8ac00a66ee78d7d1943671df8
OrangeJedi <orange.jed@outlook.com>
2018-02-22 13:47:39 -0700

    started work on visual switcher

---
+++ web/switcher.js
@@ -0,0 +1,18 @@
+var socket = io();
+socket.on('stateChange', function (msg) {
+    console.log(msg);
+    //update names
+    var txt = "<table><tr>";
+    for(var i = 0;i < 6; i++){
+        text += "<td class='programButton' id='programButton_" + i + "'>" + msg.channels[i].name + "</td>"
+    }
+    txt += "</tr></table>";
+    $('#programBus').html(txt);
+
+    txt = "<table><tr>";
+    for(i = 0;i < 6; i++){
+        text += "<td class='previewButton' id='previewButton_" + i + "'>" + msg.channels[i].name + "</td>"
+    }
+    txt += "</tr></table>";
+    $('#previewBus').html(txt);
+});
