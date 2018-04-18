c753fd08177f28f5e3c7a311015c82757869eb4b
OrangeJedi <isaac@2b4joy.com>
2018-04-17 21:14:27 -0600

    fixed bugs

--- web/aux_outputs.js
+++ web/aux_outputs.js
@@ -1,18 +1,19 @@
 var socket = io();
 var inputs = [0, 1, 2, 3, 4, 5, 6, 9, 10, 3010, 3020, 1000, 10010, 10011, 10020, 10021];
-//socket.on('stateChange', function (msg) {
-console.log(msg);
-//update names
-$('#Aux1').html(createButtons('aux1', msg));
-$('#Aux2').html(createButtons('aux2', msg));
-$('#Aux3').html(createButtons('aux3', msg));
-$('#Aux4').html(createButtons('aux4', msg));
-$('#Aux5').html(createButtons('aux5', msg));
-$('#Aux6').html(createButtons('aux6', msg));
-setButtons();
-color(msg);
+socket.on('stateChange', function (msg) {
+    console.log(msg);
+    //update names
+    $('#Aux1').html(createButtons('aux1', msg));
+    $('#Aux2').html(createButtons('aux2', msg));
+    $('#Aux3').html(createButtons('aux3', msg));
+    $('#Aux4').html(createButtons('aux4', msg));
+    $('#Aux5').html(createButtons('aux5', msg));
+    $('#Aux6').html(createButtons('aux6', msg));
+    setButtons();
+    color(msg);
 
-//});
+});
+
 function color(state) {
     $('#aux1Button_' + state.video.auxs[0]).addClass('yellow');
     $('#aux2Button_' + state.video.auxs[1]).addClass('yellow');
@@ -21,6 +22,7 @@ function color(state) {
     $('#aux5Button_' + state.video.auxs[4]).addClass('yellow');
     $('#aux6Button_' + state.video.auxs[5]).addClass('yellow');
 }
+
 function createButtons(busName, msg) {
     var b = busName[0].toUpperCase();
     for (var i = 1; i < busName.length; i++) {
@@ -37,6 +39,7 @@ function createButtons(busName, msg) {
 function setButtons() {
     $('table').find('button').css("height", $('table.bus').find('button').width());
 }
-function setAux(aux,input){
-    socket.emit('changeAux', {"aux":aux,"input":input});
+
+function setAux(aux, input) {
+    socket.emit('changeAux', {"aux": aux, "input": input});
 }
